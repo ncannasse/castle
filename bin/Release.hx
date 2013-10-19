613924446d0309be1b96d61f8c19d528d08d8657
Nicolas Cannasse <ncannasse@gmail.com>
2013-10-20 00:03:57 +0200

    release script, ignores

---
+++ bin/Release.hx
@@ -0,0 +1,74 @@
+class Release {
+	
+	public static function main() {
+		var files = [
+			"package.json",
+			"icon.png",
+			"index.html",
+			"castle.js",
+			"jquery-latest.min.js",
+			"style.css"
+		];
+		
+		var files = [for( f in files ) {
+			var data = sys.io.File.getBytes(f);
+			{
+				fileTime : Date.now(),
+				fileSize : data.length,
+				fileName : f,
+				dataSize : data.length,
+				data : data,
+				compressed : false,
+				crc32 : null,
+				extraFields : null,
+			}
+		} ];
+		var o = new haxe.io.BytesOutput();
+		new haxe.zip.Writer(o).write(Lambda.list(files));
+		var bytes = o.getBytes();
+		sys.io.File.saveBytes("package.nw", bytes);
+		
+		var platforms = [
+			"win",
+			"osx", // When unzipping with OSX, the rights will be correctly set on the .app
+			"linux", // TODO : linux uses TGZ for rights, we lack format.tgz.Writer atm
+		];
+		for( pf in platforms ) {
+			var zip = 'node-webkit-$pf.zip';
+			if( !sys.FileSystem.exists(zip) ) {
+				Sys.println('$zip not found');
+				continue;
+			}
+			var z = new haxe.zip.Reader(sys.io.File.read(zip)).read();
+			for( e in z ) {
+				switch( e.fileName ) {
+				case "nw":
+					e.fileName = "cdb";
+				case "nw.exe":
+					e.fileName = "cdb.exe";
+				case "nwsnapshot", "nwsnapshot.exe":
+					z.remove(e);
+				default:
+					if( e.fileName.substr(0, 15) == "node-webkit.app" )
+						e.fileName = "castleDB.app" + e.fileName.substr(15);
+				}
+			}
+			z.add({
+				fileTime : Date.now(),
+				fileSize : bytes.length,
+				fileName : "package.nw",
+				dataSize : bytes.length,
+				data : bytes,
+				compressed : false,
+				crc32 : null,
+				extraFields : null,
+			});
+			var out = 'castledb-$pf.zip';
+			var o = sys.io.File.write(out);
+			new haxe.zip.Writer(o).write(z);
+			o.close();
+			Sys.println('$out written');
+		}
+	}
+
+}
