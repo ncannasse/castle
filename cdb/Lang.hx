572ccaeb67f19db9f9ff8c623d737dc2a73c8aee
Nicolas Cannasse <ncannasse@gmail.com>
2017-02-21 10:52:14 +0100

    fixed html unescaping

--- cdb/Lang.hx
+++ cdb/Lang.hx
@@ -128,7 +128,7 @@ class Lang {
 		case LName(c):
 			var v = data == null ? null : data.get(c.name);
 			if( v != null )
-				Reflect.setField(o, c.name, new haxe.xml.Fast(v).innerHTML);
+				Reflect.setField(o, c.name, StringTools.htmlUnescape(new haxe.xml.Fast(v).innerHTML));
 			else {
 				var v = Reflect.field(o, c.name);
 				if( v != null && v != "" ) {
