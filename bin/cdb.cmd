53d6daf5fade1505a6abf7b407e2332f4f77e2cf
Sébastien Bénard <github@deepnight.net>
2021-04-14 22:55:30 +0200

    Updated the run script to be more user friendly (#128)

--- bin/cdb.cmd
+++ bin/cdb.cmd
@@ -1,2 +1,10 @@
-@cd %~dp0
-@nwjs\nw.exe --nwapp package.json %*
+@echo off
+
+if exist "%~dp0nwjs\nw.exe" (
+	start /D %~dp0 nwjs\nw.exe --nwapp package.json %*
+) else (
+	echo.
+	echo This requires "nw.exe" in ./nwjs/ folder.
+	echo Get it on: https://nwjs.io/
+	pause
+)
