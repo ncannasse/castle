a76e8d76ffafd4008e3a4683c8cd21921399cad5
Nicolas Cannasse <ncannasse@gmail.com>
2021-11-22 12:59:07 +0100

    added exists & find to cdb array

--- cdb/Types.hx
+++ cdb/Types.hx
@@ -48,7 +48,7 @@ private class FlagsIterator<T> {
 
 }
 
-@:forward(map, filter, indexOf)
+@:forward(map, filter, indexOf, exists, find)
 abstract ArrayRead<T>(Array<T>) from Array<T> {
 
 	public var length(get, never) : Int;
