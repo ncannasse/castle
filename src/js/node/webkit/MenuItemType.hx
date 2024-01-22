2321ac5cc9d110274a643a0232f3a54556909110
Sebastien Benard <github@deepnight.net>
2024-01-22 10:09:20 +0100

    Fixed deprecated @:enum

--- src/js/node/webkit/MenuItemType.hx
+++ src/js/node/webkit/MenuItemType.hx
@@ -1,7 +1,6 @@
 package js.node.webkit;
 
-@:enum
-abstract MenuItemType(String) {
+enum abstract MenuItemType(String) {
 	var separator = "separator";
 	var checkbox = "checkbox";
 	var normal = "normal";
