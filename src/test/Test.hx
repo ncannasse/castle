6da196251f99dcb28bd1bc49de1e2b862b38d001
trethaller <tom.rethaller@gmail.com>
2025-10-08 16:53:58 +0200

    Make test compatible with new Hide resource structure

--- src/test/Test.hx
+++ src/test/Test.hx
@@ -1,29 +1,29 @@
-import dat.Data;
+import Data;
 
 class Test {
 	
 	static function main() {
-		dat.Data.load(haxe.Resource.getString("test.cdb"));
+		Data.load(haxe.Resource.getString("test.cdb"));
 		
 		
 		
 		
-		trace(dat.Data.items.get(sword).alt.fx);
-		trace(dat.Data.items.get(sword).alt.test);
-		trace(switch( dat.Data.items.get(herb).fx ) { case Monster(m): m.id; default: null; } );
+		trace(Data.items.get(sword).alt.fx);
+		trace(Data.items.get(sword).alt.test);
+		trace(switch( Data.items.get(herb).fx ) { case Monster(m): m.id; default: null; } );
 		
-		for( s in dat.Data.monsters.resolve("wolf").skills[0].sub )
+		for( s in Data.monsters.resolve("wolf").skills[0].sub )
 			trace(s);
 		
 		
-		var sword = dat.Data.weapons.get(longsword);
+		var sword = Data.weapons.get(longsword);
 		trace("Longsword: damage=" + sword.stats.damage + ", speed=" + sword.stats.speed);
 		
-		var armor = dat.Data.armors.get(chainmail);
+		var armor = Data.armors.get(chainmail);
 		trace("Chainmail: damage=" + armor.stats.damage + ", speed=" + armor.stats.speed);
 		
-		var dagger = dat.Data.weapons.get(dagger);
-		var leather = dat.Data.armors.get(leather);
+		var dagger = Data.weapons.get(dagger);
+		var leather = Data.armors.get(leather);
 		trace("Dagger: damage=" + dagger.stats.damage + ", speed=" + dagger.stats.speed);
 		trace("Leather: damage=" + leather.stats.damage + ", speed=" + leather.stats.speed);
 	}
