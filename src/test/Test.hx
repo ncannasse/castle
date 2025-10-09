bb4dafe07c0c258a3ee737fde47c915e255bd19f
trethaller <tom.rethaller@gmail.com>
2025-10-09 09:51:18 +0200

    Allow to share common structures among tables (#172)

--- src/test/Test.hx
+++ src/test/Test.hx
@@ -1,23 +1,27 @@
-import dat.Data;
+import Data;
 
 class Test {
 	
 	static function main() {
-		#if js
-		dat.Data.load(null);
-		#else
-		dat.Data.load(haxe.Resource.getString("test.cdb"));
-		#end
+		Data.load(sys.io.File.getContent("res/data.cdb"));
 		
+		trace(Data.items.get(sword).alt.fx);
+		trace(Data.items.get(sword).alt.test);
+		trace(switch( Data.items.get(herb).fx ) { case Monster(m): m.id; default: null; } );
 		
-		
-		
-		trace(dat.Data.items.get(sword).alt.fx);
-		trace(dat.Data.items.get(sword).alt.test);
-		trace(switch( dat.Data.items.get(herb).fx ) { case Monster(m): m.id; default: null; } );
-		
-		for( s in dat.Data.monsters.resolve("wolf").skills[0].sub )
+		for( s in Data.monsters.resolve("wolf").skills[0].sub )
 			trace(s);
+		
+		
+		function checkStats(id: String, stats : Weapons_stats) {
+			trace(id + ": damage=" + stats.damage + ", speed=" + stats.speed);
+		}
+		
+		var dagger = Data.weapons.get(dagger);
+		checkStats("dagger", dagger.stats);
+
+		var armor = Data.armors.get(chainmail);
+		checkStats("chainmail", armor.stats);
 	}
 	
 }
